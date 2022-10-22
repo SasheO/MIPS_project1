971a63e3052ecb36f8c2cb4f2b098a28bac9f3f1
SasheO <76496417+SasheO@users.noreply.github.com>
2022-10-22 16:59:33 -0400

    stored number to multiply sum by in $s0, updated comments

--- project1.s
+++ project1.s
@@ -9,5 +9,14 @@ la $a0, input_str # destination memory address
 li $a1, 11 # number of input characters including null char
 syscall
 
+li $s0,1 # the number multiplying the final sum by
+
+# 0-9 ascii: 48-57
+# A-P ascii: 65-80
+# a-p ascii: 91-112
+loop:
+
+
+
 li $v0,4 # exit program
 syscall
