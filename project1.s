441deeff8619ef84a0c249fb7408fcdf13260468
SasheO <76496417+SasheO@users.noreply.github.com>
2022-10-22 16:24:17 -0400

    included comments, included syscall and static data memory reservation for reading 10 char string

--- project1.s
+++ project1.s
@@ -1,7 +1,13 @@
 .data
+input_str: .space 11 # reserve 11 bytes for input_str
 
 .text
 main:
 
-li $v0,4
+li $v0, 8 # read string
+la $a0, input_str # destination memory address
+li $a1, 11 # number of input characters including null char
+syscall
+
+li $v0,4 # exit program
 syscall
