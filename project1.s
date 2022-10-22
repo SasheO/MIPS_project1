c7cc7416654992892a7d0ea5f1d5f8939fae0592
SasheO <76496417+SasheO@users.noreply.github.com>
2022-10-22 18:33:34 -0400

    moved initial assignment of running sum register $t9 outside loop, moved assignment of upper limit into loop

--- project1.s
+++ project1.s
@@ -10,20 +10,20 @@ li $a1, 11 # number of input characters including null char
 syscall
 
 li $s0,1 # the number multiplying the final sum by
-li $s1,'9' # store ascii value of char 9 --> UPPER LIMIT
-addi $s1,$s1,1 # increment by one so that you can check for value '9, not only <9 with slti in check_0_to_9
+li $t9,0 # $t9 will store final value of sum
 
 # 0-9 ascii: 48-57
 # A-P ascii: 65-80
 # a-p ascii: 91-112
 
 loop:
-    li $t9,0 # $t9 will store final value of sum
     lb $t1,0($a0) # load character at this of string into $t1
     beq $t1,$zero, exit_loop # if null character is read, leave the loop
     addi $a0,$a0,1 # increment the address in $a0 by one to move onto next character
 
     check_0_to_9:
+        li $s1,'9' # store ascii value of char 9 --> UPPER LIMIT
+        addi $s1,$s1,1 # increment by one so that you can check for value '9, not only <9 with slti in check_0_to_9
         slti $t0,$t1,'0' # the string char in $t1 should be greater than or equal to '0' char i.e. $t0 should be 0
         bne $t0,$zero,check_a_to_p # if $t0 not 0, do the next check
         slt $t0,$t1,$s1 # check if character <= ascii code for 9 # the string char in $t1 should be less than or equal to '9' char i.e. $t0 should be 1
