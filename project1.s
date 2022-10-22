6e5f973175dd093c967cc8218d9879cea3ba478a
SasheO <76496417+SasheO@users.noreply.github.com>
2022-10-22 18:32:25 -0400

    stored upper limit for check_a_to_p in $s1
    check_0_to_9 checks if char is from 0 to 9. if not, it goes to the next check.

--- project1.s
+++ project1.s
@@ -10,6 +10,8 @@ li $a1, 11 # number of input characters including null char
 syscall
 
 li $s0,1 # the number multiplying the final sum by
+li $s1,'9' # store ascii value of char 9 --> UPPER LIMIT
+addi $s1,$s1,1 # increment by one so that you can check for value '9, not only <9 with slti in check_0_to_9
 
 # 0-9 ascii: 48-57
 # A-P ascii: 65-80
@@ -24,10 +26,9 @@ loop:
     check_0_to_9:
         slti $t0,$t1,'0' # the string char in $t1 should be greater than or equal to '0' char i.e. $t0 should be 0
         bne $t0,$zero,check_a_to_p # if $t0 not 0, do the next check
-        # FIX: this only checks for <9, but excludes char 9 itself
-        slti $t0,$t1,'9' # check if character < ascii code for 9 
-        bne $t0,1,check_a_to_p # if not, do the next check
-
+        slt $t0,$t1,$s1 # check if character <= ascii code for 9 # the string char in $t1 should be less than or equal to '9' char i.e. $t0 should be 1
+        beq $t0,$zero,check_a_to_p # if $t0 0 instead of 1, do the next check
+        # j to segment of loop that adds char value to value of $t9, the running sum
     check_a_to_p:
     check_A_to_P:
 
