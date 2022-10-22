681068506ec609d7fe7941890ad8d21705dda422
SasheO <76496417+SasheO@users.noreply.github.com>
2022-10-22 18:43:24 -0400

    included add_to_running_sum to increment running sum
    running sum in $t9 is only incremented if one of the checks passes

--- project1.s
+++ project1.s
@@ -28,10 +28,13 @@ loop:
         bne $t0,$zero,check_a_to_p # if $t0 not 0, do the next check
         slt $t0,$t1,$s1 # check if character <= ascii code for 9 # the string char in $t1 should be less than or equal to '9' char i.e. $t0 should be 1
         beq $t0,$zero,check_a_to_p # if $t0 0 instead of 1, do the next check
-        # j to segment of loop that adds char value to value of $t9, the running sum
+        j add_to_running_sum # j to segment of loop that adds char value to value of $t9, the running sum
     check_a_to_p:
     check_A_to_P:
 
+    add_to_running_sum:
+        addu $t9,$t9,$t1
+        j loop
 
 exit_loop:
 mult $t9,$t9,$s0 # store product of sum and number in $t9 
