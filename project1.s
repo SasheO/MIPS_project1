dda8a0140619904e3d47a2820e15369c148f986e
SasheO <76496417+SasheO@users.noreply.github.com>
2022-10-22 18:46:48 -0400

    updated exit_loop
    fixed mult instruction to mul, moved li $s0,1 to exit_loop so that $s0 can be used earlier in loop

--- project1.s
+++ project1.s
@@ -9,7 +9,6 @@ la $a0, input_str # destination memory address
 li $a1, 11 # number of input characters including null char
 syscall
 
-li $s0,1 # the number multiplying the final sum by
 li $t9,0 # $t9 will store final value of sum
 
 # 0-9 ascii: 48-57
@@ -29,6 +28,7 @@ loop:
         slt $t0,$t1,$s1 # check if character <= ascii code for 9 # the string char in $t1 should be less than or equal to '9' char i.e. $t0 should be 1
         beq $t0,$zero,check_a_to_p # if $t0 0 instead of 1, do the next check
         j add_to_running_sum # j to segment of loop that adds char value to value of $t9, the running sum
+    
     check_a_to_p:
     check_A_to_P:
 
@@ -37,10 +37,11 @@ loop:
         j loop
 
 exit_loop:
-mult $t9,$t9,$s0 # store product of sum and number in $t9 
-li $v0,1 # print integer
-add $a0,$t9,$zero # move final value as argument
-syscall
+li $s0,1 # the number multiplying the final sum by, n+1
+
+li $v0,1 # syscall print integer
+mul $a0,$t9,$s0 # store product of sum and number in $a0
+syscall # value of $a0 gets printed
 
 li $v0,10 # exit program
 syscall
