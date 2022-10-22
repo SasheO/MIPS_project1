d1fc2d9a13526202a3583a36d9e82a6ca4b2751b
SasheO <76496417+SasheO@users.noreply.github.com>
2022-10-22 18:48:39 -0400

    included instruction to convert ascii value to integer

--- project1.s
+++ project1.s
@@ -11,7 +11,7 @@ syscall
 
 li $t9,0 # $t9 will store final value of sum
 
-# 0-9 ascii: 48-57
+
 # A-P ascii: 65-80
 # a-p ascii: 91-112
 
@@ -27,6 +27,7 @@ loop:
         bne $t0,$zero,check_a_to_p # if $t0 not 0, do the next check
         slt $t0,$t1,$s1 # check if character <= ascii code for 9 # the string char in $t1 should be less than or equal to '9' char i.e. $t0 should be 1
         beq $t0,$zero,check_a_to_p # if $t0 0 instead of 1, do the next check
+        addi $t0,$t0,-48 # convert ascii value to integer (0-9 ascii: 48-57)
         j add_to_running_sum # j to segment of loop that adds char value to value of $t9, the running sum
     
     check_a_to_p:
