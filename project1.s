cde2b54a667d93a0f3411bc58b621bf25e1fb01d
SasheO <76496417+SasheO@users.noreply.github.com>
2022-10-25 11:57:06 -0400

    reorganize check_0_to_9 for easy readability

--- project1.s
+++ project1.s
@@ -19,12 +19,14 @@ loop:
     addi $a0,$a0,1 # increment the address in $a0 by one to move onto next character in the next loop
 
     check_0_to_9:
-        li $s1,'9' # store ascii value of char 9 --> UPPER LIMIT
-        addi $s1,$s1,1 # increment by one so that you can check for value '9, not only <9 with slti in check_0_to_9
         slti $t0,$t1,'0' # the string char in $t1 should be greater than or equal to '0' char i.e. $t0 should be 0
         bne $t0,$zero,check_a_to_p # if $t0 not 0, do the next check
+
+        li $s1,'9' # store ascii value of char 9 --> UPPER LIMIT
+        addi $s1,$s1,1 # increment by one so that you can check for value '9, not only <9 with slti in check_0_to_9
         slt $t0,$t1,$s1 # check if character <= ascii code for 9 # the string char in $t1 should be less than or equal to '9' char i.e. $t0 should be 1
         beq $t0,$zero,check_a_to_p # if $t0 0 instead of 1, do the next check
+        
         addi $t1,$t1,-48 # convert ascii value to integer (0-9 ascii: 48-57)
         j add_to_running_sum # j to segment of loop that adds char value to value of $t9, the running sum
     
