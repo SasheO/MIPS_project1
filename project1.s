88e04eb67a4ec505bfd1cc7269049c68fbcc67f0
SasheO <76496417+SasheO@users.noreply.github.com>
2022-10-25 11:59:03 -0400

    reorganized check_A_to_P for easy readability, updated comments

--- project1.s
+++ project1.s
@@ -38,18 +38,19 @@ loop:
         addi $s1,$s1,1 # increment by one so that you can check for value 'p', not only < 'p' with slti in check_a_to_p
         slt $t0,$t1,$s1 # check if character <= ascii code for 'p' # the string char in $t1 should be less than or equal to 'p' char i.e. $t0 should be 1
         beq $t0,$zero,check_A_to_P # if $t0 0 instead of 1, do the next check
-        
+
         addi $t1,$t1,-87 # convert ascii value to integer (a-p ascii: 97-112; a-p here: 10-25)
         j add_to_running_sum # j to segment of loop that adds char value to value of $t9, the running sum
 
     check_A_to_P:
-        # if none of the conditions pass, jump back to the beginning of the loop
+        slti $t0,$t1,'A' # the string char in $t1 should be greater than or equal to 'A' char i.e. $t0 should be 0
+        bne $t0,$zero,loop # if $t0 not 0, repeat the loop for the next char
+        
         li $s1,'P' # store ascii value of char P --> UPPER LIMIT
         addi $s1,$s1,1 # increment by one so that you can check for value 'P', not only < 'P' with slti in check_A_to_P
-        slti $t0,$t1,'A' # the string char in $t1 should be greater than or equal to 'A' char i.e. $t0 should be 0
-        bne $t0,$zero,loop # if $t0 not 0, repeat the loop for the next letter
         slt $t0,$t1,$s1 # check if character <= ascii code for 'P' # the string char in $t1 should be less than or equal to 'P' char i.e. $t0 should be 1
-        beq $t0,$zero,loop # if $t0 0 instead of 1, do the next check
+        beq $t0,$zero,loop # if $t0 0 instead of 1, repeat the loop for the next char
+
         addi $t1,$t1,-55 # convert ascii value to integer (A-P ascii: 65-80; A-P here: 10-25)
 
     add_to_running_sum:
