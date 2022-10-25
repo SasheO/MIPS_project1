d0324dca39856bf22cf8344cc2cd4b03c351b3cd
SasheO <76496417+SasheO@users.noreply.github.com>
2022-10-25 11:57:49 -0400

    reorganize check_a_to_p for easy readability

--- project1.s
+++ project1.s
@@ -26,17 +26,19 @@ loop:
         addi $s1,$s1,1 # increment by one so that you can check for value '9, not only <9 with slti in check_0_to_9
         slt $t0,$t1,$s1 # check if character <= ascii code for 9 # the string char in $t1 should be less than or equal to '9' char i.e. $t0 should be 1
         beq $t0,$zero,check_a_to_p # if $t0 0 instead of 1, do the next check
-        
+
         addi $t1,$t1,-48 # convert ascii value to integer (0-9 ascii: 48-57)
         j add_to_running_sum # j to segment of loop that adds char value to value of $t9, the running sum
     
     check_a_to_p:
-        li $s1,'p' # store ascii value of char p --> UPPER LIMIT
-        addi $s1,$s1,1 # increment by one so that you can check for value 'p', not only < 'p' with slti in check_a_to_p
         slti $t0,$t1,'a' # the string char in $t1 should be greater than or equal to 'a' char i.e. $t0 should be 0
         bne $t0,$zero,check_A_to_P # if $t0 not 0, do the next check
+
+        li $s1,'p' # store ascii value of char p --> UPPER LIMIT
+        addi $s1,$s1,1 # increment by one so that you can check for value 'p', not only < 'p' with slti in check_a_to_p
         slt $t0,$t1,$s1 # check if character <= ascii code for 'p' # the string char in $t1 should be less than or equal to 'p' char i.e. $t0 should be 1
         beq $t0,$zero,check_A_to_P # if $t0 0 instead of 1, do the next check
+        
         addi $t1,$t1,-87 # convert ascii value to integer (a-p ascii: 97-112; a-p here: 10-25)
         j add_to_running_sum # j to segment of loop that adds char value to value of $t9, the running sum
 
