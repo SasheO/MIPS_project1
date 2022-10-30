aeaa900c80c937941ceae3d0ee0233213dfbf53b
SasheO <76496417+SasheO@users.noreply.github.com>
2022-10-30 14:30:46 -0400

    changed chars to the ascii values (int) in check_0_to_9 and check_a_to_p
    for the unlikely case that the li instruction with a character will produce an error

--- project1.s
+++ project1.s
@@ -19,10 +19,10 @@ loop:
     addi $a0,$a0,1 # increment the address in $a0 by one to move onto next character in the next loop
 
     check_0_to_9:
-        slti $t0,$t1,'0' # the string char in $t1 should be greater than or equal to '0' char i.e. $t0 should be 0
+        slti $t0,$t1,48 # the string char in $t1 should be greater than or equal to '0' char i.e. $t0 should be 0
         bne $t0,$zero,check_a_to_p # if $t0 not 0, do the next check
 
-        li $s1,'9' # store ascii value of char 9 --> UPPER LIMIT
+        li $s1,57 # store ascii value of char 9 --> UPPER LIMIT
         addi $s1,$s1,1 # increment by one so that you can check for value '9, not only <9 with slti in check_0_to_9
         slt $t0,$t1,$s1 # check if character <= ascii code for 9 # the string char in $t1 should be less than or equal to '9' char i.e. $t0 should be 1
         beq $t0,$zero,check_a_to_p # if $t0 0 instead of 1, do the next check
@@ -31,10 +31,10 @@ loop:
         j add_to_running_sum # j to segment of loop that adds char value to value of $t9, the running sum
     
     check_a_to_p:
-        slti $t0,$t1,'a' # the string char in $t1 should be greater than or equal to 'a' char i.e. $t0 should be 0
+        slti $t0,$t1,97 # the string char in $t1 should be greater than or equal to 'a' char i.e. $t0 should be 0
         bne $t0,$zero,check_A_to_P # if $t0 not 0, do the next check
 
-        li $s1,'p' # store ascii value of char p --> UPPER LIMIT
+        li $s1,112 # store ascii value of char p --> UPPER LIMIT
         addi $s1,$s1,1 # increment by one so that you can check for value 'p', not only < 'p' with slti in check_a_to_p
         slt $t0,$t1,$s1 # check if character <= ascii code for 'p' # the string char in $t1 should be less than or equal to 'p' char i.e. $t0 should be 1
         beq $t0,$zero,check_A_to_P # if $t0 0 instead of 1, do the next check
